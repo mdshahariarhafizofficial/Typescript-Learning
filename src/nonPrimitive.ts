0ddab7b7fd3f5e5c1f28160ad5b7435102df60f6
mdshahariarhafizofficial <shahariar.works@gmail.com>
2025-11-15 19:06:31 +0600

    nonPrimitive practice done

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,27 @@
+let arr1 : string[] = ['name', 'roll'];
+arr1.push(10)
+
+let arr2 : [number, string] = [ 55, "roll" ];
+arr2.push(true);
+
+let arr3 : (string | boolean)[] = ["String", true];
+arr3.push(66);
+
+let person: {
+    name: string;
+    age: number;
+    isMarried: boolean;
+} = {
+    name: 'mofij',
+    age: 56,
+    isMarried: true,
+}
+let person1: {
+    name: string;
+    age: number;
+    isMarried: boolean;
+} = {
+    name: 'moqless',
+    age: 56,
+    isMarried: true,
+}
