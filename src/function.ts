74e8689c31d90b9add9d8520c9185d8c9d064e40
mdshahariarhafizofficial <shahariar.works@gmail.com>
2025-11-16 21:05:18 +0600

    Fundtion Practice Done

---
+++ src/function.ts
@@ -0,0 +1,8 @@
+function summation(num1 : number, num2 : number):number{
+    return num1+num2;
+}
+
+console.log(summation(5, 5));
+
+const mul = (num1: number, num2: number): number => num1*num2;
+console.log(mul(6, 2));
