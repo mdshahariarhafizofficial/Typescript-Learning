e34278215cb40036596e9ec85548d14e08f2c4a1
mdshahariarhafizofficial <shahariar.works@gmail.com>
2025-11-23 20:00:19 +0600

    type alias practice done@

---
+++ src/typeAlias.ts
@@ -0,0 +1,27 @@
+type User = {
+    name: {
+        firstName: string;
+        lastName: string
+    }
+    age: number,
+    isMarried: boolean,
+}
+
+
+const user1 : User = {
+    name: {
+        firstName: 'Noman',
+        lastName:  "Khan"
+    },
+    age: 50,
+    isMarried: true,
+}
+
+const user2 : User = {
+    name: {
+        firstName: 'Moqless',
+        lastName: 'Mia'
+    },
+    age: 23,
+    isMarried: false,
+}
